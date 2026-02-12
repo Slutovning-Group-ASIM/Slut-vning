6615f96132cb01fa20a94cdcbe977f2cdd02cd81
Sara Sundqvist <sara.hammaren@hotmail.com>
2026-02-12 14:31:40 +0100

    Refactor methods and add task editing feature
    Refactor methods to be static and add edit functionality.

--- TODO-app/Program.cs
+++ TODO-app/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyApp
@@ -48,7 +49,7 @@ public class ToDoTask
 
 
         }
-        public void DeleteTask(int id)
+        public static void DeleteTask(int id)
         {
             
             var selectedId = tasks.FirstOrDefault(p => p.Id == id); // Checks if object with id excists
@@ -72,6 +73,7 @@ public class ToDoTask
                 Console.WriteLine("[2] - Visa alla uppgifter");
                 Console.WriteLine("[3] - Markera som klar");
                 Console.WriteLine("[4] - Ta bort uppgift");
+                Console.WriteLine("[5] - Redigera uppgift");
                 Console.WriteLine("[0] - Avsluta\n");
 
                 string input = Console.ReadLine();
@@ -97,12 +99,48 @@ public class ToDoTask
                             DisplayAllTasks();
                             break;
                         case 3:
-                            // CompleteTask();
+                            int id = ChooseId();
+                            if (id != -1)
+                            {
+                                CompleteTask(id);
+                            }
                             break;
                         case 4:
-                            isRunning = false;
+                            int id2 = ChooseId();
+                            if (id2 != -1)
+                            {
+                                DeleteTask(id2);
+                            }
+                            break;
+                        case 5:
+                            Console.WriteLine("Redigera uppgift");
+                            Console.WriteLine("================\n\n");
+
+                            // Show list of tasks so the user can see/choose ID
+                            DisplayAllTasks();
+
+                            Console.Write("\nAnge ID på uppgiften du vill ändra: ");
+                            if (int.TryParse(Console.ReadLine(), out int editId))
+                            {
+                                Console.Write("Ange ny titel: ");
+                                string newTitle = Console.ReadLine();
+
+                                if (!string.IsNullOrWhiteSpace(newTitle))
+                                {
+                                    EditTask(editId, newTitle);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Titeln får inte vara tom.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ogiltigt ID.");
+                            }
                             break;
                         case 0:
+                            isRunning = false;
                             break;
 
                         default:
@@ -117,7 +155,7 @@ public class ToDoTask
             }
         }
         // 
-        public int ChooseId()
+        public static int ChooseId()
         {            
             // menu to let user either show all tasks or choose id straight away
             Console.WriteLine("Välj ett alternativ:");
@@ -163,7 +201,7 @@ public class ToDoTask
         }
 
          // CompleteTask();
-        public void CompleteTask(int id) 
+        public static void CompleteTask(int id) 
         {
 
             ToDoTask task = tasks.FirstOrDefault(task => task.Id == id);
@@ -216,6 +254,21 @@ public class ToDoTask
                 Console.WriteLine($"{tasks[i].Id,-5} {statusSymbol,-10} {tasks[i].Title}");
 
                 Console.ResetColor();
+            }
+        }
+        public static void EditTask(int id, string newTitle)
+        {
+            // Find task based on ID
+            ToDoTask taskToEdit = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (taskToEdit != null)
+            {
+                taskToEdit.Title = newTitle;
+                Console.WriteLine($"Uppgift {id} har uppdaterats till: {newTitle}");
+            }
+            else
+            {
+                Console.WriteLine($"Kunde inte hitta någon uppgift med ID: {id}");
             }
         }
 
