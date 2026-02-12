2b999b9df15dd51fd3743535e588c8f00f4a22fd
saras871 <Sara.hammaren@hotmail.com>
2026-02-12 09:17:45 +0100

    Todotask class

--- TODO-app/Program.cs
+++ TODO-app/Program.cs
@@ -9,4 +9,19 @@ namespace MyApp
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class ToDoTask
+    {
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Guid Id { get; set; }
+
+        public ToDoTask(string title)
+        {
+            Title = title;
+            IsCompleted = false;
+            Id = Guid.NewGuid();
+        }
+    }
 }
